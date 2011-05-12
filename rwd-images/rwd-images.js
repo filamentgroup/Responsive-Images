24190383dc3a4843ec55f0ed9b75725a78aeb1e1
Andy Hume <andyhume@gmail.com>
2011-05-12 01:44:30 +0100

    - Added new demo page for image_width_check demos. - Deal with .gif images loading race condition. Wrong sizes reported if images are not loaded. - Cleaned up spaces to tabs (tabs seemed more prevalent).

--- rwd-images/rwd-images.js
+++ rwd-images/rwd-images.js
@@ -7,7 +7,7 @@
 */
 (function(win){
 	//defaults / mixins
-	var	rwdi = (function(){
+	var rwdi = (function(){
 			var defaults = {
 				clientSideOnly: false,
 				widthBreakPoint: 480,
@@ -16,11 +16,11 @@
 			//mixins from rwd_images global
 			if( 'rwd_images' in win ){
 				for (var setting in win.rwd_images) {
-			        defaults[setting] = win.rwd_images[setting];
-			    }
+					defaults[setting] = win.rwd_images[setting];
+				}
 			}
 			return defaults;
-		})(),		
+		})(),
 		clientSideOnly = rwdi.clientSideOnly, 
 		widthBreakPoint = rwdi.widthBreakPoint,
 		wideload = win.screen.availWidth > widthBreakPoint,
@@ -32,15 +32,15 @@
 		//record width cookie for subsequent loads
 		recordRes = (function(){
 			var date = new Date();
-		    date.setTime(date.getTime()+(1/*1 day*/*24*60*60*1000));
-		    doc.cookie = "rwd-resolution=" + screen.availWidth + "; expires=" + date.toGMTString() +"; path=/";
+			date.setTime(date.getTime()+(1/*1 day*/*24*60*60*1000));
+			doc.cookie = "rwd-resolution=" + screen.availWidth + "; expires=" + date.toGMTString() +"; path=/";
 		})();
 
 		//if wideload is false quit now
 		if( !wideload && !rwdi.testImageWidths ){
 			return;
 		}
-		
+
 		//find and replace img elements
 		var findrepsrc = function(){
 			var imgs = doc.getElementsByTagName('img'),
@@ -49,17 +49,37 @@
 			for(var i = 0; i < il; i++){
 				var img = imgs[i],
 					fullsrc = img.getAttribute('data-fullsrc');
-					
+
 				if(fullsrc){
-					var imageSizeOk = (img.width <= img.getAttribute("width"));
-					if(rwdi.testImageWidths && imageSizeOk){
-						img.src = img.src;
+					if(!rwdi.testImageWidths) {
+						img.src = fullsrc
+					} else 
+					// If image hasn't loaded yet, add a load event listener.
+					// If we don't wait for load, image reports wrong size.
+					if (!img.complete) {
+						// Don't need to support IE event model here, because we've
+						// already waited for images.
+						img.addEventListener("load", function() {
+							this.removeEventListener("load", arguments.callee, false);
+							testAndUpdateImage(this);
+						}, false);
 					} else {
-						img.src = fullsrc;
+						testAndUpdateImage(img);
 					}
 				}
 			}
 		},
+
+		//update an image src if its width is smaller than its rendered width. 
+		testAndUpdateImage = function(img) {
+			if(img.offsetWidth <= img.getAttribute("width")){
+				// TODO: Why is this needed?
+				img.src = img.src;
+			} else {
+				img.src = img.getAttribute('data-fullsrc');
+			}
+		}
+
 		//base tag support test (returns base tag for use if support test passes)
 			//originally used in the jQuery Mobile framework, converted to plain JS in the hasjs framework, modified for use here	
 		base = (function(){
@@ -68,54 +88,54 @@
 				a = doc.createElement("a"),
 				supported = false,
 				base = head.getElementsByTagName("base")[0] || (function(){
-	                baseAdded = true;
-	                return head.insertBefore(doc.createElement("base"), head.firstChild);
-	            })();
-	         
-	        backup = !baseAdded && base.href;   
-	        //test base support before using
-            base.href = location.protocol + "//" + "x/";
-            a.href = "y";
-            //if dynamic base tag is unsupported (Firefox)
-            if( a.href.indexOf("x/y") < 0 ){
-            	if(backup){
-            		base.href = backup;
-            	}
-            	else{
-            		head.removeChild(base);
-            	}
-            	base = null;
-            }
-            else{
-            	if(clientSideOnly){
-            		//more info up top, use with caution!
-            		base.href = "javascript://";
-            	}
-            	else{
-            		base.href = dirPath +  "rwd-router/";
-            	}
-            }
-	      return base;
-	    })(),
-	    
-	    //flag for whether loop has run already
-	    complete = false,
-	    
-	    //remove base if present, find/rep image srcs if wide enough (maybe make this happen at domready?)
-	    readyCallback = function(){
-	    	if( complete ){ return; }
-	    	complete = true;
-	    	//making this async seems to ensure images don't double request?
-	    	setTimeout(function(){
-		    	if( base ) {
+					baseAdded = true;
+					return head.insertBefore(doc.createElement("base"), head.firstChild);
+				})();
+
+			backup = !baseAdded && base.href;
+			//test base support before using
+			base.href = location.protocol + "//" + "x/";
+			a.href = "y";
+			//if dynamic base tag is unsupported (Firefox)
+			if( a.href.indexOf("x/y") < 0 ){
+				if(backup){
+					base.href = backup;
+				}
+				else{
+					head.removeChild(base);
+				}
+				base = null;
+			}
+			else{
+				if(clientSideOnly){
+					//more info up top, use with caution!
+					base.href = "javascript://";
+				}
+				else{
+					base.href = dirPath +  "rwd-router/";
+				}
+			}
+		  return base;
+		})(),
+
+		//flag for whether loop has run already
+		complete = false,
+
+		//remove base if present, find/rep image srcs if wide enough (maybe make this happen at domready?)
+		readyCallback = function(){
+			if( complete ){ return; }
+			complete = true;
+			//making this async seems to ensure images don't double request?
+			setTimeout(function(){
+				if( base ) {
 					//set base back to something real before removing
 					base.href = dirPath;
 					head.removeChild(base);
 				}
 				findrepsrc();
 			},0);
-	    };
-	
+		};
+
 	//DOM-ready or onload handler
 	//W3C event model
 	if ( doc.addEventListener ) {
@@ -123,9 +143,11 @@
 		//fallback
 		win.addEventListener( "load", readyCallback, false );
 	}
-	// If IE event model is used
+	//If IE event model is used
 	else if ( doc.attachEvent ) {
-		doc.attachEvent("onreadystatechange", readyCallback );
+		//FIX: This is firing before the images are ready in the DOM in IE8,
+		//Wwich is fine for screen res check, but not image size check.
+		//doc.attachEvent("onreadystatechange", readyCallback );
 		//fallback
 		win.attachEvent( "onload", readyCallback );
 	}
